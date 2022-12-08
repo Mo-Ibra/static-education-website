4eb44ae02ad627b99c3a0231ea4a76649b75bd46
Dev-Maradona <maradona.0000@yahoo.com>
2022-12-08 23:17:25 +0200

    Create navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,42 @@
 'use strict';
 
+/**
+ * add eventListener on multiple elements
+*/
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+
+/**
+ * PRELOADER
+*/
+
+const preloader = document.querySelector("[data-preloader]");
+const circle = document.querySelector("[data-circle]");
+
+window.addEventListener("load", function() {
+    preloader.classList.add("loaded");
+    circle.style.animation = "none";
+    document.body.classList.add("loaded");
+});
+
+
+/**
+ * NAVBAR TOGGLER FOR MOBILE
+*/
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]")
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function() {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, 'click', toggleNavbar);
